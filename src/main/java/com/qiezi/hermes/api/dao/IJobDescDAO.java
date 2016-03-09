d959650c3c137fcf4e342c25e941953d9ec0f7e2
hulei02 <hulei02@meituan.com>
2016-03-09 10:47:52 +0800

    add post new job dao

--- src/main/java/com/qiezi/hermes/api/dao/IJobDescDAO.java
+++ src/main/java/com/qiezi/hermes/api/dao/IJobDescDAO.java
@@ -11,11 +11,17 @@ import java.util.List;
  */
 @Repository
 public interface IJobDescDAO {
-    public List<JobDescDO> getJobDescListBySelectParam(@Param("cityId") int cityId, @Param("areaId") int areaId, @Param("cateId") int cateId, @Param("experience") int experience, @Param("degree") int degree,@Param("salary") int salray, @Param("postTime") String postTime);
+    public List<JobDescDO> getJobDescListBySelectParam(@Param("cityId") int cityId, @Param("areaId") int areaId, @Param("cateId") int cateId, @Param("experience") int experience, @Param("degree") int degree, @Param("salary") int salray, @Param("postTime") String postTime);
 
     public JobDescDO getJobDescByJobId(@Param("jobId") int jobId);
 
-	public List<JobDescDO> getJobDescList(@Param("offset") int offset, @Param("limit") int limit);
+    public List<JobDescDO> getJobDescList(@Param("offset") int offset, @Param("limit") int limit);
 
-	public int countJob();
+    public int countJob();
+
+    public int addNewJobDesc(@Param("cityId") int cityId, @Param("areaId") int areaId, @Param("cateId") int cateId, @Param("jobName") String jobName,
+                             @Param("jobType") int jobType,@Param("salary") int salary, @Param("welfare") String welfare, @Param("experience") int experience, @Param("degree") int degree,
+                             @Param("salaryMin") int salaryMin, @Param("salaryMax") int salaryMax, @Param("jobImg") String jobImg, @Param("address") String address,
+                             @Param("longitude") double longitude, @Param("latitude") double latitude, @Param("userId") int userId,
+                             @Param("companyName") String companyName, @Param("jobContent") String jobContent, @Param("companyDesc") String company_desc);
 }
--- src/test/com/qiezi/hermes/api/dao/IJobDescDAOTest.java
+++ src/test/com/qiezi/hermes/api/dao/IJobDescDAOTest.java
@@ -1,11 +1,11 @@
 package com.qiezi.hermes.api.dao;
 
-import base.BaseTest;
+import base.BaseNonTransactionTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-public class IJobDescDAOTest extends BaseTest {
+public class IJobDescDAOTest extends BaseNonTransactionTest {
 
     @Autowired
     private IJobDescDAO jobDescDAO;
@@ -18,5 +18,12 @@ public class IJobDescDAOTest extends BaseTest {
     @Test
     public void testGetJobDetailById() throws Exception {
         System.out.println(jobDescDAO.getJobDescByJobId(10002));
+    }
+
+    // code everywhere
+    @Test
+    public void testAddNewJobDesc() throws Exception {
+        jobDescDAO.addNewJobDesc(1, 123, 10002, "销售", 0, 1000, "福利各种待遇好", 0, 0, 1000, 2000, "http://img3.imgtn.bdimg.com/it/u=1183223528,3058066243&fm=21&gp=0.jpg",
+                "北京市朝阳区望京国际研发园", 12.23, 234.34, 12345, "茄子科技有限公司", "负责给各个女生进行保研", "公司各种福利赶紧过来吧");
     }
 }

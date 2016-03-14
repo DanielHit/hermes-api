39c8a859b712c7d1e8d980c43f1c31a4b938e124
hulei02 <hulei02@meituan.com>
2016-03-14 11:31:29 +0800

    fix date transform bug

--- src/main/java/com/qiezi/hermes/api/utils/DateFormatUtils.java
+++ src/main/java/com/qiezi/hermes/api/utils/DateFormatUtils.java
@@ -1,9 +1,9 @@
 package com.qiezi.hermes.api.utils;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class DateFormatUtils {
 
@@ -23,29 +23,17 @@ public class DateFormatUtils {
     }
 
     public static long getIntervalDays(String endDate) throws ParseException {
-        java.text.SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:MM:SS");
-        java.util.Date parseEndDate = format.parse(endDate);
+        java.util.Date temp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse(endDate);
+
         System.out.println((new Date()).getTime());
-        System.out.println(parseEndDate.getTime());
-        long day = ((new Date()).getTime() - parseEndDate.getTime()) / (24 * 60 * 60 * 1000);
+        System.out.println(temp.getTime());
+        long day = ((new Date()).getTime() - temp.getTime()) / (24 * 60 * 60 * 1000);
         return day;
     }
 
     // 报价查询
     public static void main(String[] args) throws ParseException {
-        Date dt2 = new Date();
-        java.text.SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:MM:SS");
-
-        java.util.Date dt1 = format.parse("2016-02-12 10:54:54");
-        Date startDate = dt1;           // Set start date
-        Date endDate = new Date();      // Set end date
-
-        long duration = endDate.getTime() - startDate.getTime();
-        long diffInSeconds = TimeUnit.MILLISECONDS.toSeconds(duration);
-        long diffInMinutes = TimeUnit.MILLISECONDS.toMinutes(duration);
-        long diffInHours = TimeUnit.MILLISECONDS.toHours(duration);
-
-        System.out.println(duration);
-
+        System.out.println(getIntervalDays("2016-02-14 10:54:54"));
     }
 }
